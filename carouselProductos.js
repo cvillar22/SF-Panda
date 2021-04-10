0c36c7d2867dcd18d0970fa881ae4ff7ce01e9d4
cvillar22 <csorrochevillar@gmail.com>
2021-04-10 00:11:09 -0300

    index base

---
+++ carouselProductos.js
@@ -0,0 +1,14 @@
+(function($) {
+    "use strict";
+
+    // manual carousel controls
+    $('.next').click(function() {
+        $('#postsCarousel').carousel('next');
+        return false;
+    });
+    $('.prev').click(function() {
+        $('#postsCarousel').carousel('prev');
+        return false;
+    });
+
+})(jQuery);
