fcc5e6749c96cbe2400755cc869129ef1d572b92
Nicolas Ramos <ramos.nicolas@hotmail.es>
2021-05-06 18:42:04 +0200

    Implementado el Return to Top

---
+++ js/return-to-top.js
@@ -0,0 +1,9 @@
+const toTop = document.querySelector("#return-to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+});
